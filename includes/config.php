39d3f695f40a0813e79c9931cd29acc28ed0c117
Dio Brando <7iscool77@gmail.com>
2018-03-27 01:07:21 +0200

    Revert "now the 'main' site looks better."
    This reverts commit 97aee40b25b44d2850e4aad28e2d7803ee78572f.

--- includes/config.php
+++ includes/config.php
@@ -10,6 +10,6 @@ define('RESULTS_PER_PAGE', '3');
 define('HOST', 'localhost');
 define('USER', '23669_username');
 define('PASS', '23669_password');
-define('DBNAME', '23669_firstthing');
+define('DBNAME', 'myband');
 
 ?>
